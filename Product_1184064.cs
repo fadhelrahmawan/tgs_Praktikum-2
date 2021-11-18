ee8cfb7364fb4ec1ec131a1fb67b985f9a334df7
fadhelrahmawan <57004251+fadhelrahmawan@users.noreply.github.com>
2021-11-18 23:19:56 +0700

    Add files via upload

---
+++ Product_1184064.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_1_1184064
+{
+    class Product_1184064
+    {
+        private string myType;
+        private string myTitle;
+
+        public Product_1184064()
+        {
+
+        }
+
+        public Product_1184064(string type, string title)
+        {
+            this.myType = type;
+            this.myType = title;
+        }
+        public string MyType
+        {
+            get
+            {
+                return myType;
+            }
+            set
+            {
+                myType = value;
+            }
+        }
+        public string MyTitle
+        {
+            get
+            {
+                return myTitle;
+            }
+            set
+            {
+                myTitle = value;
+            }
+        }
+    }
+}
+
