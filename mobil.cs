e975695b9c4b731917fbc04b31e17f94f8c032c6
fadhelrahmawan <57004251+fadhelrahmawan@users.noreply.github.com>
2021-11-18 23:22:39 +0700

    Add files via upload

---
+++ mobil.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_2_1184064
+{
+    public class Mobil
+    {
+        private int hargaMob = 0;
+        public void HargaMie(int buy)
+        {
+
+            hargaMob = Convert.ToInt32(buy * 250000000);
+
+        }
+        public void output()
+        {
+            Console.WriteLine("Total Harganya : " + hargaMob);
+        }
+
+    }
+}
