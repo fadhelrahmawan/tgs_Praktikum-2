ae07ccf0cbb54fcfad05bfcaf7eda0c1b1dfd7c9
fadhelrahmawan <57004251+fadhelrahmawan@users.noreply.github.com>
2021-11-18 23:21:54 +0700

    Add files via upload

---
+++ DVD_1184064.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_1_1184064
+{
+    public class DVD_1184064: Product_1184064
+    {
+        protected string duration;
+
+        public DVD_1184064(string title, string duration)
+        {
+            this.MyTitle = title;
+            this.Duration = duration;
+            this.MyType = "DVD";
+
+            Console.WriteLine("Ini dari class DVD");
+        }
+        public string Duration
+        {
+            get
+            {
+                return duration;
+            }
+            set
+            {
+                duration = value;
+            }
+        }
+    }
+}
+
