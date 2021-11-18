1b0fc525e800f8ee09925cbc7b5eb529e25b61c6
fadhelrahmawan <57004251+fadhelrahmawan@users.noreply.github.com>
2021-11-18 23:22:20 +0700

    Add files via upload

---
+++ motor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_2_1184064
+{
+    public class Motor
+    {
+        private int hargaMot = 0;
+
+        public void HargaMot(int buy)
+        {
+
+            hargaMot = Convert.ToInt32(buy * 12000000);
+
+        }
+        public void output() //method untuk menampilkan hasil dari proses method HargaMot
+        {
+            Console.WriteLine("Total Harganya : " + hargaMot);
+        }
+
+    }
+}
