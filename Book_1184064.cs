304f34359fd03c1ec881ed9ca83652ea4444e7a5
fadhelrahmawan <57004251+fadhelrahmawan@users.noreply.github.com>
2021-11-18 23:21:08 +0700

    Add files via upload

---
+++ Book_1184064.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_1_1184064
+{
+    public class Book_1184064: Product_1184064
+    {
+        protected string pageCount;
+
+        public Book_1184064(string type, string title, string pagecount)
+            : base(type, title)
+            
+        {
+            this.pageCount = pagecount;
+        }
+
+        public string PageCount
+        {
+            get
+            {
+                return pageCount;
+            }
+            set
+            {
+                pageCount = value;
+            }
+        }
+    }
+}
